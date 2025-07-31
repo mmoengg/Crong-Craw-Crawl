4d2efcdc8e8bbd0b8b880aac6e30cd7ddf1ae210
mmoengg <mmoengg@gmail.com>
2025-07-31 13:48:42 +0900

    feat: 양천, 강서 가센 라우터 추가, css 수정

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # --- 금천구립도서관 ---
 def crawl_gc_book():
-    url = 'https://geumcheonlib.seoul.kr/geumcheonlib/uce/board/noticeList.do?selfId=1082'
+    url = 'https://geumcheonlib.seoul.kr/geumcheonlib/uce/programList.do?selfId=1090'
     headers = {'User-Agent': 'Mozilla/5.0'}
     
     response = requests.get(url, headers=headers)
@@ -23,11 +23,14 @@ def crawl_gc_book():
         input_tag = row.find('input', id='BOA_IDX')
         input_id = input_tag.get('value', '') if input_tag else ''
         if cols:
-            selected_cols = [cols[1], cols[2]]  # 제목, 작성자만 선택
-            selected_cols.append(input_id)
-            data.append(selected_cols)
+            category = cols[1] # 대상
+            title = cols[0]  # 제목
+            if category == '성인':
+                # '성인'인 경우만 추가
+                selected_cols = [title, input_id]
+                data.append(selected_cols)
 
-    columns = ['제목', '작성자', '링크']
+    columns = ['제목', '게시글아이디']
     df = pd.DataFrame(data, columns=columns)
     return df
 
@@ -58,6 +61,61 @@ def crawl_gc_family(max_page=3, rows=5):
     df = pd.DataFrame(data, columns=columns)
     return df
 
+# --- 양천 가족센터 ---
+def crawl_yc_family(max_page=3, rows=5):
+    url = 'https://ychc.familynet.or.kr/center/lay1/program/S295T322C451/recruitReceipt/list.do'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    data = []
+    for page in range(1, max_page + 1):
+        params = {'rows': rows, 'cpage': page}
+        response = requests.get(url, headers=headers, params=params)  # 페이지별 요청
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        items = soup.select('li.clearfix')
+        if not items:
+            print(f"Page {page}: 데이터 항목을 찾지 못했어요.")
+            continue
+
+        for item in items:
+            a_tag = item.find('a')
+            title = a_tag.get_text(strip=True) if a_tag else ''
+            link = a_tag['href'] if a_tag and 'href' in a_tag.attrs else ''
+            data.append([title, link])
+
+    columns = ['제목', '링크']
+    df = pd.DataFrame(data, columns=columns)
+    return df
+
+# --- 강서 가족센터 ---
+def crawl_gs_family(max_page=3, rows=5):
+    url = 'https://gsfc.familynet.or.kr/center/lay1/program/S295T322C451/recruitReceipt/list.do'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    data = []
+    for page in range(1, max_page + 1):
+        params = {'rows': rows, 'cpage': page}
+        response = requests.get(url, headers=headers, params=params)  # 페이지별 요청
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        items = soup.select('li.clearfix')
+        if not items:
+            print(f"Page {page}: 데이터 항목을 찾지 못했어요.")
+            continue
+
+        for item in items:
+            a_tag = item.find('a')
+            title = a_tag.get_text(strip=True) if a_tag else ''
+            link = a_tag['href'] if a_tag and 'href' in a_tag.attrs else ''
+            data.append([title, link])
+
+    columns = ['제목', '링크']
+    df = pd.DataFrame(data, columns=columns)
+    return df
+
+
 # --- 라우터 ---
 @app.route('/')
 def home():
@@ -76,6 +134,19 @@ def gcfamily():
     df = crawl_gc_family()
     notices = df.to_dict(orient='records')  # 리스트 딕셔너리 형태로 변환
     return render_template('gcfamily.html', notices=notices)
+
+@app.route('/ycfamily')
+def ycfamily():
+    df = crawl_yc_family()
+    notices = df.to_dict(orient='records')  # 리스트 딕셔너리 형태로 변환
+    return render_template('ycfamily.html', notices=notices)
+
+@app.route('/gsfamily')
+def gsfamily():
+    df = crawl_gs_family()
+    notices = df.to_dict(orient='records')  # 리스트 딕셔너리 형태로 변환
+    return render_template('gsfamily.html', notices=notices)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
